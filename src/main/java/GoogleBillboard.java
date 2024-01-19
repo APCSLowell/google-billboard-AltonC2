d31ccb3d2e645be1142832da41a888b88978c846
AltonC2 <143027301+AltonC2@users.noreply.github.com>
2024-01-18 21:32:53 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,28 +1,27 @@
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-    int start = 2;
-  int end = 12;
-	public static void main(final String[] args) {
-		  String digits = e.substring(start,end);
-  double dNum = Double.parseDouble(digits);
-  boolean isPrimeResult = isPrime(dNum);
-  if(isPrimeResult){
-  System.out.println(digits);
-  }
-  else{
-  start++;
-  end++;
-	}
-  }
-
-	//Finish this function
-	public boolean isPrime(double dNum){
+   	public static void main(final String[] args) {
 	
-    double nlength = Math.sqrt(dNum);
-    for(int i =2; i <= nlength; i++){
-     if(dNum % i == 0)
-     return false;
-    }		
-    return true;	
+	for (int i =2; i<e.length(); i++){
+String digits = e.substring(i,i+10);
+
+double dNum = Double.parseDouble(digits);
+if (isPrime(dNum)){
+
+	System.out.println(dNum);
+break;
+}
+  	 }
+}
+
+
+public static boolean isPrime(double nums){
+  if(nums < 2)
+   	 return false;
+  for(int i = 2; i <= Math.sqrt(nums); i++){
+    	if(nums % i == 0)
+     	 return false;
+  }
+  return true;
 }
 }
